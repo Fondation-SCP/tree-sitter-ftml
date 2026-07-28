0ad5ee05a9c5b6f296533297223cda6ab0f66760
thief-sty <11363816+thief-sty@users.noreply.github.com>
2026-07-28 17:26:19 +0200

    feat: add fundamental grammar rules
    freeform, block

--- grammar.js
+++ grammar.js
@@ -10,8 +10,33 @@
 export default grammar({
   name: "ftml",
 
+  externals: $ => [
+    $._start_block_name,
+    $._end_block_name
+  ],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat($._element),
+
+    _element: $ => choice(
+      $.freeform,
+      $.block
+    ),
+
+    freeform: $ => /.*/, // Text that is outside all blocks
+
+    block: $ => seq(
+      $.block_start,
+      /.*/,
+      $.block_end
+    ),
+
+    block_start: $ => seq(
+      '[[', alias($._start_block_name, $.block_name), ']]'
+    ),
+
+    block_end: $ => seq(
+      '[[/', alias($._end_block_name,  $.block_name), ']]'
+    )
   }
 });
