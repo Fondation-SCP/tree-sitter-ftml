d7b8d73bb855e94d3576f743bad0e42c52fafa34
thief-sty <11363816+thief-sty@users.noreply.github.com>
2026-07-31 14:30:47 +0200

    feat(scanner): impl scan
    Contains code adapted from tree-sitter-html, licensed under MIT (c) 2014 Max Brunsfeld

--- src/block.h
+++ src/block.h
@@ -3,10 +3,51 @@
 
 #include "tree_sitter/array.h"
 
-typedef enum { SIZE } BlockType;
+typedef enum { SIZE, NONE_ } BlockType;
 
 typedef struct {
   BlockType type;
 } Block;
+
+typedef Array(char) String;
+
+typedef struct {
+  char block_name[16];
+  BlockType block_type;
+} BlockMapEntry;
+
+static const BlockMapEntry BLOCK_TYPES_BY_BLOCK_NAME[] = {{"SIZE", SIZE}};
+
+static inline Block block_new() {
+  Block block;
+  block.type = NONE_;
+  return block;
+}
+
+static BlockType block_type_for_name(const String *block_name) {
+  for (int i = 0; i < 126; i++) {
+    const BlockMapEntry *entry = &BLOCK_TYPES_BY_BLOCK_NAME[i];
+    if (strlen(entry->block_name) == block_name->size &&
+        memcmp(block_name->contents, entry->block_name, block_name->size) ==
+            0) {
+      return entry->block_type;
+    }
+  }
+  return NONE_;
+}
+
+static inline Block block_for_name(String name) {
+  Block block = block_new();
+  block.type = block_type_for_name(&name);
+  array_delete(&name);
+
+  return block;
+}
+
+static inline bool block_eq(const Block *self, const Block *other) {
+  return self->type == other->type;
+}
+
+static inline void block_free(Block *block) {}
 
 #endif /* TREE_SITTER_FTML_BLOCK_H */
--- src/scanner.c
+++ src/scanner.c
@@ -2,6 +2,7 @@
 #include "tree_sitter/alloc.h"
 #include "tree_sitter/array.h"
 #include "tree_sitter/parser.h"
+#include <wctype.h>
 
 enum TokenType { START_BLOCK_NAME, END_BLOCK_NAME };
 
@@ -22,7 +23,71 @@ void tree_sitter_ftml_external_scanner_deserialize(void *payload,
                                                    const char *buffer,
                                                    unsigned length) {}
 
+static inline void advance(TSLexer *lexer) { lexer->advance(lexer, false); }
+
+static inline void skip(TSLexer *lexer) { lexer->advance(lexer, true); }
+
+static void pop_block(Scanner *scanner) {
+  Block popped_block = array_pop(&scanner->blocks);
+  block_free(&popped_block);
+}
+
+static String scan_block_name(TSLexer *lexer) {
+  String tag_name = array_new();
+  while (iswalnum(lexer->lookahead)) {
+    array_push(&tag_name, towupper(lexer->lookahead));
+    advance(lexer);
+  }
+  return tag_name;
+}
+
+static bool scan_start_block_name(Scanner *scanner, TSLexer *lexer) {
+  String block_name = scan_block_name(lexer);
+  if (block_name.size == 0) {
+    array_delete(&block_name);
+    return false;
+  }
+
+  Block block = block_for_name(block_name);
+  array_push(&scanner->blocks, block);
+  lexer->result_symbol = START_BLOCK_NAME;
+
+  return true;
+}
+
+static bool scan_end_block_name(Scanner *scanner, TSLexer *lexer) {
+  String block_name = scan_block_name(lexer);
+
+  if (block_name.size == 0) {
+    array_delete(&block_name);
+    return false;
+  }
+
+  Block block = block_for_name(block_name);
+  if (scanner->blocks.size > 0 &&
+      block_eq(array_back(&scanner->blocks), &block)) {
+    pop_block(scanner);
+    lexer->result_symbol = END_BLOCK_NAME;
+  } else {
+    return false;
+  }
+
+  block_free(&block);
+  return true;
+}
+
 bool tree_sitter_ftml_external_scanner_scan(void *payload, TSLexer *lexer,
                                             const bool *valid_symbols) {
-  return true;
+  Scanner *scanner = (Scanner *)payload;
+  while (iswspace(lexer->lookahead)) {
+    skip(lexer);
+  }
+
+  if ((valid_symbols[START_BLOCK_NAME] || valid_symbols[END_BLOCK_NAME])) {
+    return valid_symbols[START_BLOCK_NAME]
+               ? scan_start_block_name(scanner, lexer)
+               : scan_end_block_name(scanner, lexer);
+  }
+
+  return false;
 }
