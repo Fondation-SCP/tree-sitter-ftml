8de7eb96db1cd8a831f5fd4d5194a0a26b371fe6
thief-sty <11363816+thief-sty@users.noreply.github.com>
2026-07-31 14:43:08 +0200

    feat(scanner): impl (de)serialize
    Contains adapted external code, see d7b8d73b

--- src/scanner.c
+++ src/scanner.c
@@ -26,12 +26,65 @@ void tree_sitter_ftml_external_scanner_destroy(void *payload) {
 
 unsigned tree_sitter_ftml_external_scanner_serialize(void *payload,
                                                      char *buffer) {
-  return 0;
+  Scanner *scanner = (Scanner *)payload;
+  uint16_t block_count =
+      scanner->blocks.size > UINT16_MAX ? UINT16_MAX : scanner->blocks.size;
+  uint16_t serialized_block_count = 0;
+
+  unsigned size = sizeof(block_count);
+  memcpy(&buffer[size], &block_count, sizeof(block_count));
+  size += sizeof(block_count);
+
+  for (; serialized_block_count < block_count; serialized_block_count++) {
+    Block block = scanner->blocks.contents[serialized_block_count];
+
+    if (size + 1 >= TREE_SITTER_SERIALIZATION_BUFFER_SIZE) {
+      break;
+    }
+    buffer[size++] = (char)block.type;
+  }
+
+  memcpy(&buffer[0], &serialized_block_count, sizeof(serialized_block_count));
+  return size;
 }
 
 void tree_sitter_ftml_external_scanner_deserialize(void *payload,
                                                    const char *buffer,
-                                                   unsigned length) {}
+                                                   unsigned length) {
+  Scanner *scanner = (Scanner *)payload;
+  for (unsigned i = 0; i < scanner->blocks.size; i++) {
+    block_free(&scanner->blocks.contents[i]);
+  }
+  array_clear(&scanner->blocks);
+
+  if (length > 0) {
+    unsigned size = 0;
+    uint16_t block_count = 0;
+    uint16_t serialized_block_count = 0;
+
+    memcpy(&serialized_block_count, &buffer[size],
+           sizeof(serialized_block_count));
+    size += sizeof(serialized_block_count);
+
+    memcpy(&block_count, &buffer[size], sizeof(block_count));
+    size += sizeof(block_count);
+
+    array_reserve(&scanner->blocks, block_count);
+    if (block_count > 0) {
+      unsigned iter = 0;
+      for (iter = 0; iter < serialized_block_count; iter++) {
+        Block block = block_new();
+        block.type = (BlockType)buffer[size++];
+        array_push(&scanner->blocks, block);
+      }
+      // add zero blocks if we didn't read enough, this is because the
+      // buffer had no more room but we held more blocks.
+      for (; iter < block_count; iter++) {
+        array_push(&scanner->blocks, block_new());
+      }
+    }
+  }
+}
 
 static inline void advance(TSLexer *lexer) { lexer->advance(lexer, false); }
 
